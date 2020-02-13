b39283db859c7622a187e453410977d6a7c2096c
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2020-02-13 08:25:07 -0300

    Finalizado desenvolvimento da home desktop

--- build/js-compiler.bat
+++ build/js-compiler.bat
@@ -1,2 +1,2 @@
-java -jar compiler.jar --js "../web-magafilio/js/jquery.mask.js" --js "../web-magafilio/js/jquery-ui.min.js" --js "../web-magafilio/js/owl.carousel.js" > "../web-magafilio/js/magafilio.js"
+java -jar compiler.jar --js "../web-magafilio/js/jquery-ui.min.js" --js "../web-magafilio/js/owl.carousel.min.js" > "../web-magafilio/js/magafilio.js"
 pause
